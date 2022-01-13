f63e5e6ad4dabb755dd1a633ca89cf8c3a8c6da7
bilyweb <97169724+bily66@users.noreply.github.com>
2022-01-13 23:17:23 +0800

    新增活動區、企業專區、utm

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,78 @@
-$(function () {
-	var width = $(window).width(),
-		height = $(window).height(),
-		headerHeight = $('header').outerHeight();
-
-	$('a.smooth-scroll[href*="#"]:not([href="#"])').on("click", function() {
-		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-			var target = $(this.hash);
-			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-			if (target.length) {
-				$('html, body').animate({
-					scrollTop: (target.offset().top - headerHeight + 1)
-				}, 1000);
-				return false;
+$('.plan-carousel').slick({
+	slidesToShow: 4,
+	responsive: [
+		{
+			breakpoint: 992,
+			settings: {
+				dots: true,
+				speed: 300,
+				slidesToShow: 1,
+				adaptiveHeight: true,
+				prevArrow: '<button type="button" class="slick-prev"><span class="material-icons-round">arrow_back_ios</span></button>',
+				nextArrow: '<button type="button" class="slick-next"><span class="material-icons-round">arrow_forward_ios</span></button>',
 			}
 		}
-	});
-
-	$(window).on('resize', function () {});
-	$(window).on('scroll', function () {});
+	]
+});
+$('.share-carousel').slick({
+	slidesToShow: 5,
+	responsive: [
+		{
+			breakpoint: 992,
+			settings: {
+				dots: true,
+				speed: 300,
+				slidesToShow: 1,
+				adaptiveHeight: true,
+				prevArrow: '<button type="button" class="slick-prev"><span class="material-icons-round">arrow_back_ios</span></button>',
+				nextArrow: '<button type="button" class="slick-next"><span class="material-icons-round">arrow_forward_ios</span></button>',
+			}
+		}
+	]
+});
+$('.book-carousel').slick({
+	slidesToShow: 4,
+	prevArrow: '<button type="button" class="slick-prev"><span class="material-icons-round">arrow_back_ios</span></button>',
+	nextArrow: '<button type="button" class="slick-next"><span class="material-icons-round">arrow_forward_ios</span></button>',
+	responsive: [
+		{
+			breakpoint: 992,
+			settings: {
+				dots: true,
+				speed: 300,
+				slidesToShow: 2,
+			}
+		},
+		{
+			breakpoint: 768,
+			settings: {
+				dots: true,
+				speed: 300,
+				slidesToShow: 1,
+			}
+		}
+	]
+});
+$('.course-carousel').slick({
+	dots: true,
+	speed: 300,
+	slidesToShow: 1,
+	adaptiveHeight: true,
+	prevArrow: '<button type="button" class="slick-prev"><span class="material-icons-round">arrow_back_ios</span></button>',
+	nextArrow: '<button type="button" class="slick-next"><span class="material-icons-round">arrow_forward_ios</span></button>'
+});
+$('.enterprise-carousel').slick({
+	dots: false,
+	speed: 300,
+	slidesToShow: 1,
+	prevArrow: '<button type="button" class="slick-prev"><span class="material-icons-round">arrow_back_ios</span></button>',
+	nextArrow: '<button type="button" class="slick-next"><span class="material-icons-round">arrow_forward_ios</span></button>',
+	responsive: [
+		{
+			breakpoint: 992,
+			settings: {
+				dots: true,
+			}
+		}
+	]
 });
