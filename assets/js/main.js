50a2098f7778d9d0d1b4277260506bdff11400f5
bilyweb <97169724+bily66@users.noreply.github.com>
2022-01-19 17:37:32 +0800

    smooth-scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,16 @@
+$('a.smooth-scroll[href*="#"]:not([href="#"])').on("click", function() {
+	if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+		var target = $(this.hash);
+		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+		if (target.length) {
+			$('html, body').animate({
+				scrollTop: (target.offset().top - headerHeight + 1)
+			}, 1000);
+			return false;
+		}
+	}
+});
+
 $('.plan-carousel').slick({
 	slidesToShow: 4,
 	responsive: [
